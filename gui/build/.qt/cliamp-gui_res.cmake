f208ce9494f88e6539ceac948c6d96c9c79f2940
Bjarne Øverli <bjarne.oeverli@gmail.com>
2026-09-01 18:09:28 +0200

    chore: remove tracked GUI build artifacts

--- gui/build/.qt/cliamp-gui_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "cliamp-gui")
-set(working_dir "/home/bjarneo/Code/cliamp/gui")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/bjarneo/Code/cliamp/gui/build/.qt/cliamp-gui_res.txt")
--- gui/build/CMakeFiles/cliamp-gui_copy_qml.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/cliamp-gui_qml.txt"
-  "CMakeFiles/cliamp-gui_copy_qml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cliamp-gui_copy_qml.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
